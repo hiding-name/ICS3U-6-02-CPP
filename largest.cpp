c6ea15b74fef0c5db632c0f67ae60ebacebb9dd1
Ubuntu <ubuntu@ip-172-31-18-91.ec2.internal>
2019-12-09 01:10:57 +0000

    Initial code

---
+++ largest.cpp
@@ -0,0 +1,50 @@
+// Copyright (c) 2019 Matsuru All rights reserved.
+//
+// Created by: Matsuru Hoshi
+// Created on: Dec 2019
+// This program makes random number and finds average
+
+#include <iostream>
+#include <list>
+
+
+int LargestNumber(std::list<int> numbers) {
+    // This is finds largest number in a list
+
+    int lastNumber = 0;
+    int largest;
+
+    for (int number : numbers) {
+        if (number > lastNumber) {
+            largest = number;
+        lastNumber = number;
+        }
+    }
+    return largest;
+}
+
+main() {
+    // This is generate 10 random number from 0-10 and figures out averag
+
+    // Sets seed as time
+    srand(time(NULL));
+
+    // variables
+    int repeater;
+    int number;
+    int largest;
+    std::list<int> numbers;
+
+    // process
+    for (repeater = 0; repeater < 10; repeater++) {
+        number = rand() % 100 + 1;
+        numbers.push_back(number);
+        // output
+        std::cout << number << std::endl;
+        largest = LargestNumber(numbers);
+    }
+
+
+    // output
+    std::cout << "\nThe largest is " << largest << std::endl;
+}
